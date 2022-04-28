5cead20eeb6f981424a829b490cdf41cb96707e2
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-04-28 17:08:32 +0600

    heroku testing

---
+++ __steps_deploy_heroku.js
@@ -0,0 +1,15 @@
+/***
+ * -----------------------
+ * ONLY ONE FOR A COMPUTER
+ * -----------------------
+ * 1.Create an account to signup.heroku.com
+ * 2.veryfy email and set password
+ * 3.download heroku cli app and install this app
+ * 4.get ready a server project in the cmd and paste the code>>>> heroku login
+ * ..........................
+ * ONECE for every project
+ * ..........................
+ * 1.heroku create
+ * 2.make sure you have done:git add .>git commit -m"">git push
+ * 3.git push heroku main
+ */
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello from root')
+    res.send('Heroku is running')
 })
 
 
